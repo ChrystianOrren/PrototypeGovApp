90cb732cf2db08fe401f17f3e39df988a02dd1cd
adambaj123 <abaj1@stetson.edu>
2024-12-02 14:00:37 -0500

    Started Inquiry Screen

--- govApp/app/(tabs)/Inquiry.tsx
+++ govApp/app/(tabs)/Inquiry.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Inquiry() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{color:'red'}}>Inquiry Screen</Text>
+    <View style={styles.Top_container}>
+      <Text style={{color:'red', fontSize: 20}}>Inquiry Screen</Text>
+      <Text style={{color: 'red'}}>Insert City Name strives to make your living experience the best it could be. We would greatly appreciate your 
+        comments and or concerns to our beloved city and we have created this form in order for you to connect with a city employee. Please fill this out to the best
+        of your ability and we will get back to you as soon as possible with your Case.   </Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  Top_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: '5%'
+  },
+  Middle_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: '5%'
+  }
+
+
+});

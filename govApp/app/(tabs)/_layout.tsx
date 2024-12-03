9637dc8dfccacbadcfdfa9a57c7ea682b33176d8
ChrystianOrren0 <orren.chris@gmail.com>
2024-12-03 12:20:01 -0500

    Simpler Tab scheme

--- govApp/app/(tabs)/_layout.tsx
+++ govApp/app/(tabs)/_layout.tsx
@@ -1,24 +1,22 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
 import { HapticTab } from '@/components/HapticTab';
-
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Import Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faCalendar, faHome, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  let iconColor = 'red'
+
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'gray',
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
@@ -35,7 +33,7 @@ export default function TabLayout() {
         options={{
           title: 'Inquiry',
           tabBarIcon: ({ color }) => (
-            <FontAwesomeIcon icon={faPencilAlt} size={28} color={color} />
+            <FontAwesomeIcon icon={faPencilAlt} size={28} color={iconColor} />
           ),
         }}
       />
@@ -44,7 +42,7 @@ export default function TabLayout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => (
-            <FontAwesomeIcon icon={faHome} size={28} color={color} />
+            <FontAwesomeIcon icon={faHome} size={28} color={iconColor} />
           ),
         }}
       />
@@ -53,7 +51,7 @@ export default function TabLayout() {
         options={{
           title: 'Calendar',
           tabBarIcon: ({ color }) => (
-            <FontAwesomeIcon icon={faCalendar} size={28} color={color} />
+            <FontAwesomeIcon icon={faCalendar} size={28} color={iconColor} />
           ),
         }}
       />

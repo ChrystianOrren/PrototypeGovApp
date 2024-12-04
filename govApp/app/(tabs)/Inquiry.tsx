ce6b20ef47b439cd07e483e3e51c5a64b5fe2014
ChrystianOrren0 <orren.chris@gmail.com>
2024-12-04 11:51:46 -0500

    Alternate Inquiry Screen
    Different ux

--- govApp/app/(tabs)/Inquiry.tsx
+++ govApp/app/(tabs)/Inquiry.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, ScrollView, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import {Dropdown} from 'react-native-element-dropdown';
 
 const data = [
@@ -10,6 +10,9 @@ const data = [
   {label: 'Technical Issues', value: 5},
 ]
 
+const submitInquiry = () => {
+    console.log("submitting")
+}
 
 
 export default function Inquiry() {
@@ -17,49 +20,56 @@ export default function Inquiry() {
   const[isFocus, setIsFocus] = useState(false);
 
   return (
-    <ScrollView style = {styles.BackgroundView}><View style={styles.Top_container}>
-      <Text style={{color:'white', fontSize: 20, paddingVertical: 10, marginBottom: 10, fontWeight:'bold'}}>Inquiry Screen</Text>
-      <Text style={{color:'white', fontWeight: 'bold'}}>Palm Coast strives to make your living experience the best it could be. We would greatly appreciate your 
-        comments and or concerns to our beloved city and we have created this form in order for you to connect with a city employee. Please fill this out to the best
-        of your ability and we will get back to you as soon as possible with your Case.   </Text>
-    </View>
-    <View style ={styles.Middle_container}>
-      <Text style={styles.MainText}> Name:</Text>
-      <TextInput
-        style={styles.Small_TextInput}
-        placeholder="Insert your first and last name"
-        placeholderTextColor={'#2b87b5'}></TextInput>
-        
-      <Text style={styles.MainText}> Email:</Text>
-      <TextInput
-        style={styles.Small_TextInput}
-        placeholder="Insert your email address"
-        placeholderTextColor={'#2b87b5'}>
-        </TextInput>
+    <ScrollView style = {styles.BackgroundView}>
 
-        <Text style={styles.MainText}> Category:</Text>
-        <Dropdown
-            style={styles.Dropdown}
-            placeholderStyle={{color: 'white'}}
-            placeholder='Select a Category'
-            selectedTextStyle={{color:'white'}}
-            iconColor='white'
-            data={data} labelField={'label'} valueField={'value'} value={value} onChange={item => {setValue(item.value);}}
-        />
-        <Text style={styles.MainText}> Description of Problem or Comment:</Text>
-        <TextInput
-        style={styles.Big_TextInput}
-        placeholder="Insert your comment or concern"
-        numberOfLines={5}
-        multiline
-        placeholderTextColor={'#2b87b5'}></TextInput>
-      </View>
-      <View style = {styles.Bottom_Container}>
-        <Button
-        title = "Submit"
-        color= '#2b87b5'
-        />
-      </View>
+        <View style={styles.Top_container}>
+            <Text style={{color:'#2b87b5', fontSize: 40, paddingTop: 40, marginBottom: 5, fontWeight:'bold'}}>Create an Inquiry...</Text>
+            <Text style={{color:'#2b87b5', fontSize: 12, marginBottom: 10}}>
+                Palm Coast is dedicated to enhancing your living experience. 
+                Share your comments or concerns using this form to connect with a city employee. 
+                Fill it out, and we'll respond to your case as soon as possible.
+            </Text>
+        </View>
+
+        <View style ={styles.Middle_container}>
+            <Text style={styles.MainText}> Name:</Text>
+            <TextInput
+                style={styles.Small_TextInput}
+                placeholder="Insert your first and last name"
+                placeholderTextColor={'#2b87b5'}>
+            </TextInput>
+        
+            <Text style={styles.MainText}> Email:</Text>
+            <TextInput
+                style={styles.Small_TextInput}
+                placeholder="Insert your email address"
+                placeholderTextColor={'#2b87b5'}>
+            </TextInput>
+
+            <Text style={styles.MainText}> Category:</Text>
+            <Dropdown
+                style={styles.Dropdown}
+                placeholderStyle={{color: 'white'}}
+                placeholder='Select a Category'
+                selectedTextStyle={{color:'white'}}
+                iconColor='white'
+                data={data} labelField={'label'} valueField={'value'} value={value} onChange={item => {setValue(item.value);}}
+            />
+            <Text style={styles.MainText}> Description of Problem or Comment:</Text>
+            <TextInput
+                style={styles.Big_TextInput}
+                placeholder="Insert your comment or concern"
+                numberOfLines={5}
+                multiline
+                textAlignVertical="top"
+                placeholderTextColor={'#2b87b5'}>
+            </TextInput>
+        </View>
+        <View style = {styles.Bottom_Container}>
+            <TouchableOpacity style={styles.button} onPress={() => submitInquiry()}>
+                <Text style={styles.buttonText}>Submit Inquiry</Text>
+            </TouchableOpacity>
+        </View>
     </ScrollView>
   );
 };
@@ -68,11 +78,10 @@ const styles = StyleSheet.create({
   Top_container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     paddingHorizontal: '5%',
-    paddingTop: "10%",
+    paddingTop: "5%",
     paddingBottom: "5%",
-    backgroundColor: '#2b87b5',
   },
   Middle_container: {
     flex: 1,
@@ -83,7 +92,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: '#2b87b5',
     fontWeight: 'bold',
-    marginVertical: 10,
+    marginVertical: 0,
     fontFamily: 'Poppins-Regular'
   },
   Small_TextInput:{
@@ -91,7 +100,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     color: '#2b87b5',
     backgroundColor: 'white',
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
+    marginBottom: '8%',
+    borderRadius: 15
   },
   Big_TextInput:{
     height: 200,
@@ -100,23 +111,39 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingHorizontal: 10,
     borderColor: '2b87b5',
+    borderRadius: 15
   },
   Dropdown: {
     backgroundColor: '#2b87b5',
     height: 50,
-    paddingHorizontal:'5%',
+    paddingHorizontal: 10,
     color: '#2b87b5',
-    textDecorationColor: 'white'
+    textDecorationColor: 'white',
+    marginBottom: '8%',
+    borderRadius: 15
   },
   Bottom_Container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: '5%',
-    marginTop: '5%'
+    padding: '5%',
+    marginTop: '3%',
+    width: '100%'
   },
   BackgroundView: {
     backgroundColor: 'white'
+  },
+  button: {
+    backgroundColor: '#ffc323',
+    padding: '4%',
+    width: '60%',
+    borderRadius: 50,
+    elevation: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
-
 });
--- govApp/app/(tabs)/index.tsx
+++ govApp/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 

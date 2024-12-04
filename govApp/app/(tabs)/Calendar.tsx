1877bd7b1382c6698bf858626231d799fc3e2118
adambaj123 <abaj1@stetson.edu>
2024-12-04 13:42:14 -0500

    added Modal to Calendar

--- govApp/app/(tabs)/Calendar.tsx
+++ govApp/app/(tabs)/Calendar.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { CalendarList } from 'react-native-calendars';
-
+import { Modal } from 'react-native';
 const parksandrecEvents = { key: 'ParksandRecEvents', color: 'blue', nameofEvent: 'Yawn' }
 
-
 export default function Calendar() {
+  const [modalIsOpen, setIsOpen] = useState(false)
+
   // Define the marked dates, using a key that matches your event's date
   const [markedDates, setMarkedDates] = useState({
     '2025-01-05': { dots: [parksandrecEvents], selected: true }
@@ -15,7 +16,7 @@ export default function Calendar() {
     console.log('Selected day:', day);
     // Check if the clicked day has any events
     const event = markedDates[day.dateString];
-
+    setIsOpen(true)
     if (event && event.dots && event.dots.length > 0) {
       // Log the event details
       console.log('Event details:', event.dots[0]); // Accessing the first event in the dots array
@@ -25,6 +26,7 @@ export default function Calendar() {
   };
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
       <CalendarList
         markedDates={markedDates} // Use the markedDates state here
         minDate={'2025-01-01'}
@@ -47,6 +49,37 @@ export default function Calendar() {
           textMonthFontSize: 20
         }}
       />
+      <Modal
+        animationType='fade'
+        transparent={true}
+        visible={modalIsOpen}
+        onRequestClose={() => {
+          setIsOpen(!modalIsOpen)
+        }}
+      >
+        <View style={styles.centerdView}>
+          <View style={styles.modalView}>
+            <Text> GoobaGoo</Text>
+            <Pressable
+              onPress={() => setIsOpen(!modalIsOpen)}>
+              <Text>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  centerdView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalView: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    elevation: 5
+  }
+})
